6f964279fefde1c36d96a4613a2c884f68addd23
Alexey Litovchenko <alex.lit@outlook.com>
2017-03-21 15:20:47 +0300

    Правки и обесчвечивание карты

--- src/partials/object-map/object-map.js
+++ src/partials/object-map/object-map.js
@@ -1,39 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    /*
-    |--------------------------------------------------------------------------
-    | YANDEX MAP
-    |--------------------------------------------------------------------------
-    |
-    | Интерактивная карта
-    |
-    */
-    if (document.getElementsByClassName('object-map').length) {
+    ymaps.ready()
+        .done(function (ym) {
+            var map = new ym.Map('object-map-widget', {
+                center: [44.49540857460564,34.16389149999997],
+                zoom: 17
+            }, {
+                searchControlProvider: 'yandex#search'
+            });
 
 
-        ymaps.ready(function () {
-            var placemark, center;
             var query = [
-                'Россия, Республика Крым, Ялта, Садовая улица, 2 ',
-                'Россия, Республика Крым, Ялта, Садовая улица, 4 ',
-                'Россия, Республика Крым, Ялта, Садовая улица, 6 ',
-                'Россия, Республика Крым, Ялта, Садовая улица, 8 ',
-                'Россия, Республика Крым, Ялта, Садовая улица, 10 ',
-                'Россия, Республика Крым, Ялта, Садовая улица, 3 ',
+                'Россия, Республика Крым, Ялта, Садовая улица, 2',
+                'Россия, Республика Крым, Ялта, Садовая улица, 4',
+                'Россия, Республика Крым, Ялта, Садовая улица, 6',
+                'Россия, Республика Крым, Ялта, Садовая улица, 8',
+                'Россия, Республика Крым, Ялта, Садовая улица, 10',
+                'Россия, Республика Крым, Ялта, Садовая улица, 3',
             ];
-
-            var map = new ymaps.Map('object-map-widget', {
-                zoom: 17,
-                center: [50, 50],
-                behaviors: ['drag', 'zoomControl', 'scrollZoom', 'typeSelector']
-            });
-
-            map.controls.add('zoomControl');
-
-            map.controls.add('mapTools', {
-                left: 35,
-                top: 5
-            });
 
             $.each(query, function (i, addr) {
                 ymaps.geocode(addr, {
@@ -52,13 +36,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
                         map.geoObjects.add(placemark);
 
-                        map.setCenter(coords);
+                        map.setBounds(map.geoObjects.getBounds());
                     }
                 });
             });
+
         });
-
-    }
-
 
 });
